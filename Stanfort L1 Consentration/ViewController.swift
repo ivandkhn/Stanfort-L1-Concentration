3c21f02a8554b3d1baad8ceb36cb2659de35a983
johndahrah <dahrah@yandex.ru>
2018-06-05 19:32:59 +0300

    Initial Commit

---
+++ Stanfort L1 Consentration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stanfort L1 Consentration
+//
+//  Created by Иван Дахненко on 05.06.2018.
+//  Copyright © 2018 John Dahrah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
